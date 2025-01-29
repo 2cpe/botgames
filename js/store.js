4fda8a6a9cf2d37b9ce16f84c228a26342c5265a
8HRK <131083330+2cpe@users.noreply.github.com>
2025-01-30 01:14:38 +0300

    Delete js directory

--- js/store.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const storeGrid = document.querySelector('.store-grid');
-    
-    // Render all products
-    storeGrid.innerHTML = products.map(product => 
-        ProductRenderer.renderProductCard(product)
-    ).join('');
-}); 
