a6837075bc2928661e60f812b3ffbf3c910a530c
8HRK <131083330+2cpe@users.noreply.github.com>
2025-01-30 10:18:03 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// This file should be listed in .gitignore
-const CONFIG = {
-    SUPABASE_URL: 'https://celxtalkhvcdymfjvjsd.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlbHh0YWxraHZjZHltZmp2anNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxODk5ODEsImV4cCI6MjA1Mzc2NTk4MX0.rRWpT5jVoHdLtbZhI_FoqykXulJXpbiJWBc6o9TV0Z4'
-}; 
--- js/config.sample.js
+++
@@ -1,5 +0,0 @@
-// Rename this file to config.js and add your actual keys
-const CONFIG = {
-    SUPABASE_URL: 'your_supabase_url_here',
-    SUPABASE_KEY: 'your_supabase_key_here'
-}; 
